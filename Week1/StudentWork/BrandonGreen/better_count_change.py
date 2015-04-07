39f656f19827f7a03f6b0d3aef8c0269636a11f6
indigo-spacefish <brandong85@gmail.com>
2015-04-07 16:42:20 -0700

    new WIP

---
+++ Week1/StudentWork/BrandonGreen/better_count_change.py
@@ -0,0 +1,12 @@
+__author__ = 'brandon'
+
+pocket_inventory = {
+    'nickels': 2,
+    'dimes': 10,
+    'quarters': 1
+}
+#135.0
+
+def better_counter(coin_dict):
+    totals = {}
+    for coin in coin_dict:
