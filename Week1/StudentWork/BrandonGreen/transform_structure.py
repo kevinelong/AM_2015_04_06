a2a05480e4ab7d215b6ca7776147db2b9bacd525
indigo-spacefish <brandong85@gmail.com>
2015-04-07 12:32:11 -0700

    Pre Lunch Progress, DOES NOT WORK!!

--- Week1/StudentWork/BrandonGreen/transform_structure.py
+++ Week1/StudentWork/BrandonGreen/transform_structure.py
@@ -16,11 +16,13 @@ def transform(input_list, key_field):
     for item in input_list:
         result_item = []
         result_key = ''
-        if item.keys() == key_field:
-            result_key = item[key_field]
-        else:
-            result_item.append(item)
+        for key in item.keys():
+            if key == key_field:
+                result_key = item[key]
+            else:
+                result_item.append(item)
         result[result_key] = result_item
-    return result
+        return result
+
 
 print transform(d, 'id')
