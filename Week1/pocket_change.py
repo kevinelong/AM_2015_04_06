1f84f1fdc134f43e30546c757e1922bf778cb783
Letta Raven <acuriousgirl13@yahoo.com>
2015-04-08 09:29:27 -0700

    init commit

--- Week1/pocket_change.py
+++ Week1/pocket_change.py
@@ -9,6 +9,14 @@ pocket_change = {
 def add_up(pocket_change):
     totals = {}
     # ... DO YOUR WORK HERE
+
+def main():
+    cost = input("Enter the Purchase cost of all items:")
+    tendered = input("Enter the Tendered amount:")
+    change = tendered - cost
+    change = int(change * 100)
+    a = change / 200
+
     return totals
 
 if __name__ == "__main__":
