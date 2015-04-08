f91683fd902643edbf9bfc46f1bcc6e1386fecd0
seveneleven7711 <this.mint@gmail.com>
2015-04-08 09:46:16 -0700

    second cut

---
+++ Week1/StudentWork/BrookHindley/pocketchange.py
@@ -0,0 +1,27 @@
+__author__ = 'coder'
+
+dict1 = {"nickels": 5,
+        "dimes" : 10,
+        "quarters" : 25,
+        "pennies" : 1,};
+
+raw_input  = ("How many nickels are in your pocket?");
+raw_input = ();
+print raw_input
+raw_input = ("How many dimes are in your pocket?");
+raw_input = ();
+print raw_input
+raw_input = ("How many quarters are in your pocket?");
+raw_input = ();
+2+1+2/100 =
+
+
+
+
+
+
+
+
+
+
+
