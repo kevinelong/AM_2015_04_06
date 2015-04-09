7255fed8394b0d1f2bbe4ec7bbacb2d7d2288949
Letta Raven <acuriousgirl13@yahoo.com>
2015-04-09 11:11:29 -0700

    ?

--- Week1/stocks.py
+++ Week1/stocks.py
@@ -12,8 +12,79 @@ test_data = [
 
 #CREATE TWO NEW LISTS ONE FOR EACH STOCK TICKER SYMBOL e.g. APPL and MSFT
 
-appl = []
+# appl = []
+# msft = []
+#
 
-msft = []
+#result example
+
+appl = [
+    ["2014-06-01", "APPL", 100.11],
+    ["2014-06-02", "APPL", 110.61],
+    ["2014-06-03", "APPL", 120.22],
+    ["2014-06-04", "APPL", 100.54],
+    ]
+
+
+msft = [
+    ["2014-06-01", "MSFT", 20.46],
+    ["2014-06-02", "MSFT", 21.25],
+    ["2014-06-03", "MSFT", 32.53],
+    ["2014-06-04", "MSFT", 40.71],
+    ]
+
+print appl
+print msft
+
+#result example
+
+appl = [
+    ["2014-06-01", "APPL", 100.11],
+    ["2014-06-02", "APPL", 110.61],
+    ["2014-06-03", "APPL", 120.22],
+    ["2014-06-04", "APPL", 100.54],
+    ]
+
+
+msft = [
+    ["2014-06-01", "MSFT", 20.46],
+    ["2014-06-02", "MSFT", 21.25],
+    ["2014-06-03", "MSFT", 32.53],
+    ["2014-06-04", "MSFT", 40.71],
+    ]
+
+#Example Output to eliminate redundant data
+
+stocks = {
+    "appl" = [
+    ["2014-06-01", "APPL", 100.11],
+    ["2014-06-02", "APPL", 110.61],
+    ["2014-06-03", "APPL", 120.22],
+    ["2014-06-04", "APPL", 100.54],
+    ],
+
+"msft": = [
+    ["2014-06-01", "MSFT", 20.46],
+    ["2014-06-02", "MSFT", 21.25],
+    ["2014-06-03", "MSFT", 32.53],
+    ["2014-06-04", "MSFT", 40.71],
+    ] }
+
+
 
 #ONCE THAT WORKS THEN what would need to change to copy with an unknown number of stock ticker symbols?
+stocks = {}
+SYMBOL_INDEX = 1
+DATE_INDEX = 0
+VALUE_INDEX = 2
+
+for s  in test_data:
+    symbol = s{SYMBOL_INDEX}
+    if symbol not in stock.keys():
+        stocks[symbol] = []
+    day = s[DATE_INDEX]
+    value = s[VALUE_INDEX]
+    stock = [day, value]
+    stocks[symbol].append(stock)
+
+print(stocks)
