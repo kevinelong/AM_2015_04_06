58cdaab945862fe306baa0e29ab82690c54ea7c2
Brook <this.mint@gmail.com>
2015-04-09 12:07:39 -0700

    bad first stab

---
+++ Week1/StudentWork/BrookHindley/MakeTea.py
@@ -0,0 +1,79 @@
+# Tea Kettle
+
+def kettle ();
+    # this is the kettle to be filled with water
+
+def faucet ();
+    # faucet to be used to fill kettle
+
+def stove burner ();
+    # this is the stove burner used to heat the water
+
+def tea(types)[1 "Decaf" : 2 "Caf"]);
+    # types of tea (decaf or caf)
+
+def run_faucet[3 "Cold" : 4 "Hot"];
+    # user runs the faucet
+
+if run_faucet[4]
+    return "You must use cold water to make tea, dummy!"
+    #user runs faucet in hot triggering error message
+
+if run_faucet[3]
+    return "Don't let the kettle overflow!"
+    #user runs Cold faucet triggering reminder to not overfill
+
+
+
+
+
+
+
+
+
+# make tea
+#
+# kettle .. microwave
+# water
+#     goal temp
+#     Qty
+#
+# Tea
+#     Qty
+#     preferred type
+#     steep time minutes
+#
+# Steps:
+#
+# 1. fill kettle
+#     if kitchen
+#         a. Go to sink
+#         b. turn on cold water
+#         c. place kettle below the faucet
+#         while kettle not full
+#         wait
+#         if water full break
+#         Remove kettle
+#             turn off water
+#                 turn off stove
+#
+# 2. Place kettle on stove
+#     turn on burner (which)
+# while not at goal temp wait
+#
+#     public method (make tea)
+
+# milk? sugar? lemon?
+#
+# (type [] ) sugarlumps 0-N
+
+# Board Game
+#
+# tic tac toe
+# classes = board (rows) game pieces x type o type blank (presence or absense of a piece)
+# players would have a name property (ordinal property e.g. player 1 or 2 or color)
+# 1. build or clear board
+# 2. Notify player it's their turn'
+# 3. Give first player pieces of type x
+# 4. Get input from player (which row, column they want to place in)
+
