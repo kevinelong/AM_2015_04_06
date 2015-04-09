4fbe043d7d3ebc8720b9ef625530aae97c9eae4f
Sam Baron <sambaron@ownmail.net>
2015-04-09 15:41:00 -0700

    Create player turn
    Still need win condition and full board test

--- Week1/StudentWork/SamBaron/game_classes.py
+++ Week1/StudentWork/SamBaron/game_classes.py
@@ -154,6 +154,47 @@ class Game(object):
         self.display_board()
 
 
+    def input_row(self):
+        input_row = input("Please enter the row you want to select --> ")
+        while input_row.isdigit() == False or int(input_row) < 1 or int(input_row) > self.board.rows:
+            input_row = input("That row number isn't on the board.  Please enter the row you want to select --> ")
+        return input_row
+
+
+    def input_column(self):
+        input_column = input("Please enter the column you want to select --> ")
+        while input_column.isdigit() == False or int(input_column) < 1 or int(input_column) > self.board.columns:
+            input_column = input("That column number isn't on the board.  Please enter the column you want to select --> ")
+        return input_column
+
+
+    def position_is_empty(self, position):
+        if position.value == self.board.blank_symbol:
+            return True
+        else:
+            return False
+
+
+    def player_turn(self, player):
+        print("Hey {}, it's your turn".format(player.name))
+
+        input_row = self.input_row()
+        input_column = self.input_column()
+        turn_position = self.board.get_position(input_row, input_column)
+        while not self.position_is_empty(turn_position):
+            print("THAT POSITION IS NOT EMPTY")
+            input_row = self.input_row()
+            input_column = self.input_column()
+            turn_position = self.board.get_position(input_row, input_column)
+
+        #TODO Add win condition
+        turn_position.value = player.value
+
+        print("Here's the new board")
+        self.display_board()
+
+
+
     def play_game(self):
 
         # Setup Game
@@ -173,27 +214,12 @@ class Game(object):
 
         self.welcome_message()
 
+        end_game = False
 
-        # Loop through players
-        for player in players:
-            print("Hey {}, it's your turn".format(player.name))
-            # User Input Row        #TODO Check integer digits "x.isdigit()"
-            row = int(input("Please enter the row you want to select --> "))
-            while row < 1 or row > self.board.rows:
-                row = int(input("That row number isn't on the board.  Please enter the row you want to select --> "))
-            # User Input Column
-            column = int(input("Please enter the column you want to select --> "))
-            while column < 1 or column > self.board.columns:
-                column = int(input("That column number isn't on the board.  Please enter the column you want to select --> "))
+        while not end_game:
+            for player in players:
+                end_game = self.player_turn(player)
 
-            # Get position on board
-            position = self.board.get_position(row, column)
-            # Set position on board using player value
-            position.value = player.value
-
-            # Print Board
-            print("Here's the new board")
-            self.board.draw_board()
 
 
 if __name__ == "__main__":
