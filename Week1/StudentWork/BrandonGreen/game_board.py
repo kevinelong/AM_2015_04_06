c42b615cd8a2026afadc4502080992ac5bf871f1
indigo-spacefish <brandong85@gmail.com>
2015-04-09 11:51:44 -0700

    Format is correct

--- Week1/StudentWork/BrandonGreen/game_board.py
+++ Week1/StudentWork/BrandonGreen/game_board.py
@@ -9,16 +9,10 @@ game_state = [
 
 def draw():
     for row in game_state:
-        print row
-
-    game_on = '.' in game_state
-    turn = 1
-    if turn % 2 == 0:
-        whose_turn = 'Player 1'
-    else:
-        whose_turn = 'Player 2'
-
-    #while game_on:
+        line = []
+        for column in row:
+            line += column
+        print line
 
 
 # EXAMPLE OUTPUT
@@ -30,4 +24,3 @@ game_state[1][1] = 'X'
 game_state[0][0] = 'O'
 
 draw()
-
