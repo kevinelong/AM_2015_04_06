bec112ad6caa1d00ade9eb05579571eeaf01c29c
kevinelong <S!mpl312C0mplexity>
2015-04-09 09:54:32 -0700

    stocks

--- Week1/stocks.py
+++ Week1/stocks.py
@@ -1,4 +1,4 @@
-#GIVEN THE FOLLOWING LIST OF LIST
+# GIVEN THE FOLLOWING LIST OF LIST
 test_data = [
     ["2014-06-01", "APPL", 100.11],
     ["2014-06-02", "APPL", 110.61],
@@ -10,6 +10,19 @@ test_data = [
     ["2014-06-04", "MSFT", 40.71],
 ]
 
+#RESULT EXAMPLE
+appl = [
+    ["2014-06-01", "APPL", 100.11],
+    ["2014-06-02", "APPL", 110.61],
+    ["2014-06-03", "APPL", 120.22],
+    ["2014-06-04", "APPL", 100.54],
+]
+msft = [
+    ["2014-06-01", "MSFT", 20.46],
+    ["2014-06-02", "MSFT", 21.25],
+    ["2014-06-03", "MSFT", 32.53],
+    ["2014-06-04", "MSFT", 40.71],
+]
 #CREATE TWO NEW LISTS ONE FOR EACH STOCK TICKER SYMBOL e.g. APPL and MSFT
 
 appl = []
@@ -17,3 +30,20 @@ appl = []
 msft = []
 
 #ONCE THAT WORKS THEN what would need to change to copy with an unknown number of stock ticker symbols?
+
+#EXAMPLE OUTPUT
+stocks = {
+    "appl": [
+        ["2014-06-01",  100.11],
+        ["2014-06-02",  110.61],
+        ["2014-06-03",  120.22],
+        ["2014-06-04",  100.54],
+    ],
+    "msft": [
+        ["2014-06-01", 20.46],
+        ["2014-06-02", 21.25],
+        ["2014-06-03", 32.53],
+        ["2014-06-04", 40.71],
+    ]
+}
+
