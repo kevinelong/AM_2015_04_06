495a8805aa04dc61e03489f9983d224dfb40fb88
maddencs <maddencs@gmail.com>
2015-04-09 14:53:05 -0700

    pocket change calculator v2.1

--- Week1/StudentWork/CoryMadden/tictactoe.py
+++ Week1/StudentWork/CoryMadden/tictactoe.py
@@ -4,37 +4,22 @@ game_state = ['blank', '.', '.', '.', '.', '.', '.', '.', '.', '.']
 
 win_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
-
 def draw():
     print game_state[1:4]
     print game_state[4:7]
     print game_state[7:10]
 
-
 draw()
 
-
-def check_winX():
-    for x,y,z in win_combos:
+def check_win(sym):
+    for x, y, z in win_combos:
         win_sum = 0
-        for digit in x,y,z:
-            if game_state[digit] == 'X':
+        for digit in x, y, z:
+            if game_state[digit] == sym:
                 win_sum += 1
                 if win_sum == 3:
                     print "You win!"
                     draw()
-                    quit()
-            else:
-                pass
-
-def check_winO():
-    for x,y,z in win_combos:
-        win_sum = 0
-        for digit in x,y,z:
-            if game_state[digit] == 'O':
-                win_sum += 1
-                if win_sum == 3:
-                    print "You win!"
                     quit()
             else:
                 pass
@@ -45,13 +30,13 @@ class Player(object):
         self.piece = 'X'
 
     def place_piece(self, number):
-        place_range = range(0,10)
+        place_range = range(0, 10)
         if number in place_range:
             if game_state[number] == '.':
                 game_state[number] = self.piece
-        else:
-            print "That's an invalid location."
-            self.place_piece(int(raw_input("Try another spot: ")))
+            else:
+                print "That's an invalid location."
+                self.place_piece(int(raw_input("Try another spot: ")))
 
 
 Player1 = Player(raw_input("Player 1 name: "))
@@ -63,12 +48,13 @@ while turns <= 9:
     try:
         if ((turns % 2 != 0) or turns == 1):
             Player1.place_piece(int(raw_input((Player1.name + " place piece: "))))
-            check_winX()
+            check_win('X')
             draw()
+            turns += 1
         else:
             Player2.place_piece(int(raw_input((Player2.name + " place piece: "))))
-            check_winO()
+            check_win('O')
             draw()
-        turns += 1
+            turns += 1
     except ValueError:
         print "Input a number between 1 and 9: "
