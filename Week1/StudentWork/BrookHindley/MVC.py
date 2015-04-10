5a1c0fe01482f07274ab0f498d53d8300edb7e40
Brook <this.mint@gmail.com>
2015-04-10 14:05:03 -0700

    second stab

---
+++ Week1/StudentWork/BrookHindley/MVC.py
@@ -0,0 +1,100 @@
+__author__ = 'coder'
+# MVC: A GENERIC ARCHITECTURE FOR MAKING APPS THAT DISPLAY DATA
+
+# MODEL: A LIST OF OBJECTS. TYPICALLY FROM A DATABASE
+class Model(object):
+    def __init__(self, name, fields):
+        self.name = name
+        self.fields = fields
+        self.objects = []
+
+    def create(self, item):
+        self.objects.append(item)
+
+
+# VIEW: A TEMPLATE FOR A PAGE OR PAGE FRAGMENT
+class View(object):
+    def __init__(self, template, model):
+        self.template = template
+        self.model = model
+
+    def render(self):
+        output = ""
+        for item in self.model.objects:
+            item_template = self.template
+            for field in self.model.fields:
+                if field in item.keys():
+                    item_template = item_template.replace("{{" + field + "}}", item[field])
+            output += item_template
+        return output
+
+
+# CONTROLLER: Routes messages
+class Controller(object):
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        return self.routes[path].render()
+
+
+# CONTAINS THE SINGLE CONTROLLER AND ALL MODEL AND VIEW INSTANCES
+class Application():
+    def __init__(self):
+        self.models = {}
+        self.views = {}
+        self.controller = Controller()
+
+# CREATE AN APPLICATION INSTANCE
+app = Application()
+
+# define models (
+app.models["user"] = Model("user", ["name", "score"])
+app.models["game"] = Model("game", ["game_name", "description"])
+
+# load model objects form database tables
+app.models["user"].objects = [
+    {"name": "Bob", "score": "9"},
+    {"name": "Carol", "score": "11"},
+    {"name": "Ted", "score": "15"},
+    {"name": "Alice", "score": "13"}]
+
+score_template = "\nHello <em>{{name}}</em>, your score is <strong>{{score}}</strong>.<br>\n"
+scores_view = View(score_template, app.models["user"])
+
+app.controller.routes = {
+    "/scores/": scores_view,
+    "/score/": scores_view,
+}
+
+request_path = "/scores/"
+print(app.controller.route(request_path))
+
+# TODO:
+# 1. Add a new model, view/template and route)
+# 2. call your new route and write output to a file
+# 3. open file in your browser
+
+class Model(object):
+    def __init__(self, age, height)
+        self.age = age
+        self.height = height
+        self.objects
+
+    def create (self, item):
+        self.object.append(item)
+
+class View(object):
+    def __init__(self, template, model):
+        self.template = template
+        self.model = model
+
+    def render(self):
+        output = ""
+        for item in self.model.objects:
+            item_template = self.template
+            for field in self.model.fields:
+                if field in item.keys():
+                    item_template = item_template.replace("{{" + field + "}}", item[field])
+
+
