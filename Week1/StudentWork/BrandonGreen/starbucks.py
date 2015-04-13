3dc28c1b72bbb37e86aec17cf7d567c892059429
indigo-spacefish <brandong85@gmail.com>
2015-04-12 22:55:24 -0700

    Working code

--- Week1/StudentWork/BrandonGreen/starbucks.py
+++ Week1/StudentWork/BrandonGreen/starbucks.py
@@ -3,33 +3,16 @@ data = [[531, 408, 'A'], [225, 52, 'B'], [594, 242, 'C'], [351, 102, 'D'], [371,
         [587, 88, 'M'], [347, 180, 'N'], [180, 332, 'O'], [250, 522, 'P'], [88, 475, 'Q'], [260, 128, 'R'],
         [328, 505, 'S'], [381, 201, 'T'], [360, 192, 'U'], [414, 313, 'V'], [525, 293, 'W'], [240, 563, 'X'],
         [117, 546, 'Y'], [507, 127, 'Z']]
+
 square_size = 600
+
 style = """
 <style>
     #box { background:blue; position:relative; }
     #box span { color:white; position:absolute; }
 </style>
 """
-f = open("positions.html", "w")
-f.write(style)
-f.write('<div id="box" style="width:{0}px;height:{0}px;">\n'.format(square_size))
-for item in data:
-    f.write('<span style="left:{x}px; top:{y}px;"> {v} </span>'.format(x=item[0], y=item[1], v=item[2]))
-f.write("</div>\n")
-f.close()
 
-# TODO NEAREST "STARBUCKS"
-# 1. Define a function that for arbitrary value e.g. "J" find the nearest N items e.g. 5
-#   a. calculate the distance to each neighbor
-#   b. sort the list by that distance
-#   c. return the top N from the sorted list
-# 2. Modify the draw code to highlight "top" items
-#   a. add a style property for color
-#   b. determine if this item is in the top 5
-#   c. conditionally set the color bases on if its the top 5
-#   d. also indicate the selected position e.g. "J" with another color
-
-# OUTPUT html for "J" and 5 would show R D N U and T highlighted.
 
 import math
 
@@ -51,8 +34,27 @@ def nearest_starbucks(input_data, selected_point):
 
     sorted_distances = sorted(distances_list, key=lambda l: l[0])
     result = sorted_distances[0:5]
+    result_keys = []
 
-    return result
+    for data_point in result:
+        result_key = data_point[1]
+        result_keys.append(result_key)
+
+    f = open("positions.html", "w")
+    f.write(style)
+    f.write('<div id="box" style="width:{0}px;height:{0}px;">\n'.format(square_size))
+
+    for item in data:
+        item_key = item[2]
+        if item_key in result_keys:
+            f.write('<span style="left:{x}px; top:{y}px; background-color: yellow; color: black"> {v} </span>'.format(x=item[0], y=item[1], v=item[2]))
+        elif item_key in selected_key:
+            f.write('<span style="left:{x}px; top:{y}px; background-color: green"> {v} </span>'.format(x=item[0], y=item[1], v=item[2]))
+        else:
+            f.write('<span style="left:{x}px; top:{y}px;"> {v} </span>'.format(x=item[0], y=item[1], v=item[2]))
+
+    f.write("</div>\n")
+    f.close()
 
 
-print(nearest_starbucks(data, data[0]))
+nearest_starbucks(data, data[7])
