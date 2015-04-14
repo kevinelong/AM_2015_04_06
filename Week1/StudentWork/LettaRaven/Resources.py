e638c34e42594fe6a7eb38248614836dea104389
Letta Raven <acuriousgirl13@yahoo.com>
2015-04-14 13:02:31 -0700

    Kindle Resources

---
+++ Week1/StudentWork/LettaRaven/Resources.py
@@ -0,0 +1,21 @@
+__author__ = 'LettaRaven'
+Here are some free Kindle books that might be helpful
+
+You Dont't Know JS http://www.amazon.com/You-Dont-Know-JS-Going-ebook/dp/B00V20DQU8/ref=sr_1_2?s=digital-text&ie=UTF8&qid=1429041043&sr=1-2
+
+Python For Informatics http://www.amazon.com/Python-Informatics-Information-Charles-Severance-ebook/dp/B00K0O8HFQ/ref=sr_1_15?s=digital-text&ie=UTF8&qid=1429041388&sr=1-15
+
+Thinking in Javascript http://www.amazon.com/Thinking-JavaScript-Aravind-Shenoy-ebook/dp/B00JUI6LUQ/ref=sr_1_30?s=digital-text&ie=UTF8&qid=1429041438&sr=1-30
+
+What is Node? http://www.amazon.com/What-Node-Brett-McLaughlin-ebook/dp/B005ISQ7JC/ref=sr_1_37?s=digital-text&ie=UTF8&qid=1429041483&sr=1-37
+
+Bash Command Line http://www.amazon.com/Bash-Command-Line-Pro-Tips-ebook/dp/B00K53EO3G/ref=sr_1_42?s=digital-text&ie=UTF8&qid=1429041514&sr=1-42
+
+Thinking in CSS http://www.amazon.com/Bash-Command-Line-Pro-Tips-ebook/dp/B00K53EO3G/ref=sr_1_42?s=digital-text&ie=UTF8&qid=1429041514&sr=1-42
+
+Thinking in HTML http://www.amazon.com/Thinking-HTML-Aravind-Shenoy-ebook/dp/B00NN8H4FU/ref=sr_1_53?s=digital-text&ie=UTF8&qid=1429041576&sr=1-53
+
+CSS and Documents http://www.amazon.com/CSS-Documents-Eric-Meyer-ebook/dp/B008RBQHTW/ref=sr_1_68?s=digital-text&ie=UTF8&qid=1429041645&sr=1-68
+
+Coding Interview Ninja http://www.amazon.com/Coding-Interview-Ninja-questions-interview-ebook/dp/B00VCPJ7C4/ref=sr_1_74?s=digital-text&ie=UTF8&qid=1429041666&sr=1-74
+
