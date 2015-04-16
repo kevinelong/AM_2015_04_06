a3c946da765f67fbb1e8f7b8c1bd9058e42579a7
kevinelong <S!mpl312C0mplexity>
2015-04-16 15:48:02 -0700

    javascript examples redux

---
+++ Week2/list_and_dict_types.js
@@ -0,0 +1,19 @@
+
+// Array in JavaScript == List in Python
+
+var my_list = [11, 22, 33]; // Array/List
+
+// Object in JavaScript == Dict in Python
+
+var my_dict = {id: 123, name: "BOB"};
+
+//NOT USING THE OBJECT LITERAL SHORTHAND
+
+var list = new Array(); // SAME as []
+list.push(11);
+list.push(22);
+list.push(33);
+
+var dict = new Object(); //SAME as {}
+dict.id = 123;
+dict.name = "BOB";
