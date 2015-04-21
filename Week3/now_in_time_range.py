ebe3f81df0276f72fab9e2cf1a06f862e7313fcc
kevinelong <S!mpl312C0mplexity>
2015-04-21 15:07:08 -0700

    time in range

--- Week3/now_in_time_range.py
+++ Week3/now_in_time_range.py
@@ -7,9 +7,10 @@ def in_range(test, start, stop):
             return True
     return False
 
+
 if __name__ is "__main__":
     test = datetime.now().time()
     start = datetime(1970, 1, 1, 9, 0).time()
     stop = datetime(1970, 1, 1, 16, 0).time()
-    print(test,start,stop)
+    print(test, start, stop)
     print(in_range(test, start, stop))
