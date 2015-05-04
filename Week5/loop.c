0176efe2c2e3589fbbe609ece0881a67166170d5
kevinelong <S!mpl312C0mplexity>
2015-05-04 12:51:09 -0700

    main c demo loop

---
+++ Week5/loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //printf
+
+int main() {
+
+    for(int i = 0; i < 20; i++) {
+        if(i<10){
+            printf("Loop 0%d. Hello, World!\n", i);
+        }else{
+            printf("Loop %d. Hello, World!\n", i);
+        }
+    }
+
+    return 0;
+}
+
+
