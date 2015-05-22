3d9ff382a33f34e98d6d3c7e21db6177de6f4f6a
indigo-spacefish <brandong85@gmail.com>
2015-05-22 14:03:24 -0700

    no more off by 1!

--- Week7/deck_brandon.py
+++ Week7/deck_brandon.py
@@ -4,25 +4,31 @@
 
 from random import randint
 
+
 def shuffle(input_list):
     deck = input_list
+    cards_left = (len(deck)-1)
+    random_int = randint(0, cards_left)
+
     result = []
 
-    for card in range(0, len(input_list)):
-        cards_left = len(deck)
-        random_int = randint(0, cards_left)
+    while cards_left:
 
         if deck[random_int] not in result:
-            deck.pop(random_int)
             result.append(deck[random_int])
+            deck = [x for x in deck if x not in result]
+            cards_left = (len(deck)-1)
+            random_int = randint(0, cards_left)
+        else:
+            random_int = randint(0, cards_left)
 
     return result
-
 
 suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
 card_values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
 
-def make_deck (suits, card_values):
+
+def make_deck(suits, card_values):
     result = []
     for suit in suits:
         for value in card_values:
@@ -32,6 +38,8 @@ def make_deck (suits, card_values):
 DECK = make_deck(suits, card_values)
 
 print shuffle(DECK)
+
+
 
 
 
