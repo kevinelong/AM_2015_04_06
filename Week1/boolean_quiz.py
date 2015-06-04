5f8f12e79b0d5ffdcccc36c3bc3cd81691627d29
kevinelong <S!mpl312C0mplexity>
2015-06-04 14:04:44 -0700

    refactored as a class and a list

--- Week1/boolean_quiz.py
+++ Week1/boolean_quiz.py
@@ -1,64 +1,71 @@
 import random
 
-boolean_expressions = {
-    'not False': not False,
-    'not True': not True,
-    'True or False': True or False,
-    'True or True': True or True,
-    'False or True': False or True,
-    'False or False': False or False,
-    'True and False': True and False,
-    'True and True': True and True,
-    'False and True': False and True,
-    'False and False': False and False,
-    'not (True or False)': not (True or False),
-    'not (True or True)': not (True or True),
-    'not (False or True)': not (False or True),
-    'not (False or False)': not (False or False),
-    'not (True and False)': not (True and False),
-    'not (True and True)': not (True and True),
-    'not (False and True)': not (False and True),
-    'not (False and False)': not (False and False),
-    '1 != 0': 1 != 0,
-    '1 != 1': 1 != 1,
-    '0 != 1': 0 != 1,
-    '0 != 0': 0 != 0,
-    '1 == 0': 1 == 0,
-    '1 == 1': 1 == 1,
-    '0 == 1': 0 == 1,
-    '0 == 0': 0 == 0,
-}
-
-SCORE = 0
+boolean_expressions = [
+    'not False',
+    'not True',
+    'True or False',
+    'True or True',
+    'False or True',
+    'False or False',
+    'True and False',
+    'True and True',
+    'False and True',
+    'False and False',
+    'not (True or False)',
+    'not (True or True)',
+    'not (False or True)',
+    'not (False or False)',
+    'not (True and False)',
+    'not (True and True)',
+    'not (False and True)',
+    'not (False and False)',
+    '1 != 0',
+    '1 != 1',
+    '0 != 1',
+    '0 != 0',
+    '1 == 0',
+    '1 == 1',
+    '0 == 1',
+    '0 == 0'
+]
 
 
-def random_question():
-    global boolean_expressions
-    question = random.choice(list(boolean_expressions.keys()))
-    answer = boolean_expressions[question]
+class ExpressionGame():
+    def __init__(self, expression_list):
+        self.expression_list = expression_list
+        self.player_score = 0
 
-    return question, answer
+    def get_random_question(self):
+        index = random.randint(0, len(self.expression_list)-1)
+        question = self.expression_list[index]
+        answer = eval(question)
+        return index, question, answer
+
+    def play(self):
+        print('''
+            Welcome to the boolean value game!
+            For each question type spell out the whole answer (without quotes).
+            The answer will be either "True" or "False".
+        ''')
+        length = len(boolean_expressions)
+
+        while len(boolean_expressions) > 0:
+            remaining = len(boolean_expressions)
+            print(
+                "{0} of {1} answered correctly.".format(self.player_score, length),
+                "{0} remain.".format(remaining)
+            )
+            index, question, answer = self.get_random_question()
+            player_answer = input("What is the value of the following expression?\n\n\t{0}\n".format(question))
+
+            if player_answer == str(answer):
+                print("\nThat is correct!\n")
+                del self.expression_list[index]
+                self.player_score += 1
+            else:
+                print('Incorrect. The correct answer is: ' + str(answer))
+        print("FINAL SCORE: {0} of {1} questions".format(self.player_score, length))
 
 
-def game():
-    global boolean_expressions, SCORE
-    print('Welcome to the boolean value game!')
-    print('Type True or False for each question.')
-    length = len(boolean_expressions)
-    while len(boolean_expressions) > 0:
-        left = len(boolean_expressions)
-        print("You have answered {0} of {1} questions correctly.".format(SCORE, length))
-        print("There are %d questions left." % left)
-        question = random_question()
-        player_answer = input("What is the value of {0}?: ".format(question[0]))
-
-        if player_answer == str(question[1]):
-            print("That is correct!")
-            del boolean_expressions[question[0]]
-            SCORE += 1
-        else:
-            print('Incorrect.')
-    print("FINAL SCORE: {0} of {1} questions".format(SCORE, length))
-
-
-game()
+g = ExpressionGame(boolean_expressions)
+g.play()
