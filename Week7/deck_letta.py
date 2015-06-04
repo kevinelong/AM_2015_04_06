8a2ceb482a921e82bafe581c8d7e4ce4985aeef4
kevinelong <S!mpl312C0mplexity>
2015-06-04 11:47:32 -0700

    updates

--- Week7/deck_letta.py
+++ Week7/deck_letta.py
@@ -1,11 +1,11 @@
 __author__ = 'Letta'
 
-#I did Tarot suits
+# I did Tarot suits
 
 import itertools, random
 
-deck =list(itertools.product(range(1,14),['Wands', 'Cups', 'Swords', 'Coins']))
+deck = list(itertools.product(range(1, 14), ['Wands', 'Cups', 'Swords', 'Coins']))
 random.shuffle(deck)
-print ("You got:")
+print("You got:")
 for i in range(9):
     print(deck[i][0], 'of', deck[i][1])
