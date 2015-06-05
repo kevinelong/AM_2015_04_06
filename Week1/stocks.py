fcd24890eb55d44b328d15b411bc9c433544fd25
kevinelong <S!mpl312C0mplexity>
2015-06-05 14:46:35 -0700

    updates

--- Week1/stocks.py
+++ Week1/stocks.py
@@ -1,4 +1,4 @@
-#GIVEN THE FOLLOWING LIST OF LIST
+# GIVEN THE FOLLOWING LIST OF LIST
 test_data = [
     ["2014-06-01", "APPL", 100.11],
     ["2014-06-02", "APPL", 110.61],
@@ -10,81 +10,66 @@ test_data = [
     ["2014-06-04", "MSFT", 40.71],
 ]
 
-#CREATE TWO NEW LISTS ONE FOR EACH STOCK TICKER SYMBOL e.g. APPL and MSFT
+# CREATE TWO NEW LISTS ONE FOR EACH STOCK TICKER SYMBOL e.g. APPL and MSFT
 
 # appl = []
 # msft = []
 #
 
-#result example
+# result example
 
 appl = [
     ["2014-06-01", "APPL", 100.11],
     ["2014-06-02", "APPL", 110.61],
     ["2014-06-03", "APPL", 120.22],
     ["2014-06-04", "APPL", 100.54],
-    ]
-
+]
 
 msft = [
     ["2014-06-01", "MSFT", 20.46],
     ["2014-06-02", "MSFT", 21.25],
     ["2014-06-03", "MSFT", 32.53],
     ["2014-06-04", "MSFT", 40.71],
-    ]
+]
 
-print appl
-print msft
+print
+appl
+print
+msft
 
-#result example
+# result example
 
 appl = [
     ["2014-06-01", "APPL", 100.11],
     ["2014-06-02", "APPL", 110.61],
     ["2014-06-03", "APPL", 120.22],
     ["2014-06-04", "APPL", 100.54],
-    ]
-
+]
 
 msft = [
     ["2014-06-01", "MSFT", 20.46],
     ["2014-06-02", "MSFT", 21.25],
     ["2014-06-03", "MSFT", 32.53],
     ["2014-06-04", "MSFT", 40.71],
-    ]
+]
 
-#Example Output to eliminate redundant data
+# Example Output to eliminate redundant data
 
 stocks = {
-    "appl" = [
-    ["2014-06-01", "APPL", 100.11],
-    ["2014-06-02", "APPL", 110.61],
-    ["2014-06-03", "APPL", 120.22],
-    ["2014-06-04", "APPL", 100.54],
+    "appl": [
+        ["2014-06-01", "APPL", 100.11],
+        ["2014-06-02", "APPL", 110.61],
+        ["2014-06-03", "APPL", 120.22],
+        ["2014-06-04", "APPL", 100.54],
     ],
-
-"msft": = [
-    ["2014-06-01", "MSFT", 20.46],
-    ["2014-06-02", "MSFT", 21.25],
-    ["2014-06-03", "MSFT", 32.53],
-    ["2014-06-04", "MSFT", 40.71],
-    ] }
+    "msft": [
+        ["2014-06-01", "MSFT", 20.46],
+        ["2014-06-02", "MSFT", 21.25],
+        ["2014-06-03", "MSFT", 32.53],
+        ["2014-06-04", "MSFT", 40.71],
+    ]
+}
 
 
 
 #ONCE THAT WORKS THEN what would need to change to copy with an unknown number of stock ticker symbols?
-stocks = {}
-SYMBOL_INDEX = 1
-DATE_INDEX = 0
-VALUE_INDEX = 2
-
-for s  in test_data:
-    symbol = s{SYMBOL_INDEX}
-    if symbol not in stock.keys():
-        stocks[symbol] = []
-    day = s[DATE_INDEX]
-    value = s[VALUE_INDEX]
-    stock = [day, value]
-    stocks[symbol].append(stock)
-
-print(stocks)
